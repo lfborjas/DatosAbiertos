f5ff8774b198e78396c046acf1167721aeb83e04
Byron Sommardahl <byron@sommardahl.com>
2013-04-15 18:33:13 -0700

    Created the rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenData::Application.config.secret_token = 'c8a9febab2f1d5e1cdd322fb6419f2900888a2fbbb8f27f2c6a0466c6a8a4a215e4cd3868de704628143c880517fee0c34a7be08b1b475f50cd2b9fc3b669262'
