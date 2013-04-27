7f8f0269702e0be4d9fe24f4202f118c66edfb3a
Luis Borjas <luisfborjas@gmail.com>
2013-04-27 14:05:55 -0600

    Adds failing spec for the feeds controller
    In this step, instead of driving with cucumber, we go down one level and
    test with a spec for the controller: we expect it to "assign" (set for
    the view) an instance variable that contains the existing feeds.

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe FeedsController do
-
   describe "GET 'index'" do
-    it "returns http success" do
+    it "returns the existing feeds" do
+      existing_feeds = 2.times.collect{ FactoryGirl.create(:feed) }  
       get 'index'
-      response.should be_success
+      assigns(:feeds).should eq existing_feeds
     end
   end
-
 end
