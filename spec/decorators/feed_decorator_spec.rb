b9aebf7f32b802d9081a978edcf81ee2facc8ddc
Luis Borjas <luisfborjas@gmail.com>
2013-04-27 14:31:45 -0600

    makes the feed decorator spec fail

--- spec/decorators/feed_decorator_spec.rb
+++ spec/decorators/feed_decorator_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe FeedDecorator do
+  let(:feed){ FactoryGirl.create(:feed, name: "dinosaurs") }
+  let(:decorated_feed){ feed.decorate }
+
+  describe "#name" do
+    it "should return a titleized version of the name" do
+      decorated_feed.name.should eq "Dinosaurs"
+    end
+  end
 end
